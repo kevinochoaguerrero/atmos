2e0050f64ebee2ff21e9b4b515ec304ea91cbf6b
kevinochoaguerrero <kevin.ochoa@stellarisoft.com>
2024-10-05 22:49:09 -0700

    update: mapbox integration

---
+++ src/app/components/map.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map: React.FC = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = `${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
+
+    if (mapContainerRef.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/kevtrosky/cm1x36zc6002701pu07ek9x72',
+   center: [-102.5528, 23.6345], 
+        zoom: 5, 
+      });
+
+      return () => {
+        map.remove();
+      };
+    }
+  }, []);
+
+  return <div className="map-container" ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default Map;
---
+++ src/app/map/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from 'react';
+import Map from '@/app/components/map'; // Ajusta la ruta según tu estructura de carpetas
+
+const Home = () => {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+};
+
+export default Home;
